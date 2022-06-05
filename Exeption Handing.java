401fa239ada9d435c9ed0eb8db474a3ce545a1e7
Aish-Rai <78042325+Aish-Rai@users.noreply.github.com>
2022-06-05 12:42:28 +0530

    Add files via upload

---
+++ Exeption Handing.java
@@ -0,0 +1,15 @@
+// Java Program to Demonstrate How Exception Is Thrown
+
+// Class
+// ThrowsExecp
+class GFG {
+
+	// Main driver method
+	public static void main(String args[])
+	{
+		// Taking an empty string
+		String str = null;
+		// Getting length of a string
+		System.out.println(str.length());
+	}
+}
---
+++ Fisrt Java Program.java
@@ -0,0 +1,26 @@
+
+Java is one of the most popular and widely used programming languages and platforms.
+Java is fast, reliable, and secure. Java is used in every nook and corner from desktop to web applications, scientific supercomputers to gaming consoles, cell phones to the Internet.
+
+Java is easy to learn, and its syntax is simple and easy to understand. It is based on C++ (so easier for programmers who know C++).
+
+The process of Java programming can be simplified in three steps: 
+
+Create the program by typing it into a text editor and saving it to a file – HelloWorld.java.
+Compile it by typing “javac HelloWorld.java” in the terminal window.
+Execute (or run) it by typing “java HelloWorld” in the terminal window.
+The below-given program is the most simple program of Java printing “Hello World” to the screen. Let us try to understand every bit of code step by step.
+
+
+
+// A Java program to print "Hello World" 
+public class GFG { 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello World"); 
+    } 
+}
+
+
+Output 
+Hello, World
